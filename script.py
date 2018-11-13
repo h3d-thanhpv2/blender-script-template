7c39dc52c460d1d7254f716d6be53a50e81c2c33
Thanh Pham Van <phamthanh_cis60@outlook.com>
2018-11-13 15:12:31 +0700

    Add more things

--- script.py
+++ script.py
@@ -9,9 +9,39 @@ if exporter_dir is None:
 elif exporter_dir not in sys.path:
     sys.path.append(exporter_dir)
 
+
+class ExportMesh(object):
+
+    def __init__(self, mesh):
+        assert mesh.type == "MESH", 'parameter object is not a mesh'
+        self.mesh = mesh
+
+    @property
+    def vertices(self):
+        return self.mesh.data.vectices
+
+    @property
+    def normals(self):
+        return self.mesh.data.vectices
+
+    @property
+    def polygons(self):
+        return self.mesh.data.polygons
+
+    @property
+    def tessfaces(self):
+        if not self.mesh.data.tessfaces and self.polygons:
+            self.mesh.data.calc_tessface()
+
+        return self.mesh.data.tessfaces
+
+
 if __name__ == "__main__":
     objects = bpy.data.objects
 
     for obj in objects:
         if obj.type == "MESH":
-            print(obj.name)
+            ex_mesh = ExportMesh(obj)
+
+            for face in ex_mesh.tessfaces:
+                print(str(face))
